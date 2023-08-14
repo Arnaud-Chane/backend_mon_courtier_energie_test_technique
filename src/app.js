a4b1ea28e76ed64762518a614828a51fb83b17b3
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-14 15:57:16 +0200

    Adding router to app.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,14 @@
-// import some node modules for later
 const path = require("node:path");
-
-// create express app
-
 const express = require("express");
+const router = require("./router");
 const { corsMiddleware } = require("./middlewares/cors");
 
 const app = express();
 
-// use some application-level middlewares
 app.use(corsMiddleware);
-
 app.use(express.json());
-
-// serve the `backend/public` folder for public resources
+app.use("/api", router);
 
 app.use(express.static(path.join(__dirname, "../public")));
-
-// ready to export
 
 module.exports = app;
