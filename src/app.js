b2825001a78edfb86c11e3348aa3e3777757c8a6
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-14 15:39:30 +0200

    Adding cors to allow get post put delete

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ const path = require("node:path");
 // create express app
 
 const express = require("express");
+const { corsMiddleware } = require("./middlewares/cors");
 
 const app = express();
 
 // use some application-level middlewares
+app.use(corsMiddleware);
 
 app.use(express.json());
 
