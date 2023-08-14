68d6e37a3a01ebf3f5c233169dc84e4ef19e87e4
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-14 15:24:57 +0200

    Adding AddTask

--- src/models/TaskManager.js
+++ src/models/TaskManager.js
@@ -4,6 +4,20 @@ class TaskManager extends AbstractManager {
   constructor() {
     super({ table: "task" });
   }
+
+  addTask(tasks) {
+    return this.database.query(
+      `insert into ${this.table} ( user_id, title, detail, task_done, task_archived ) values (?, ?, ?, ?, ?)`,
+      [
+        tasks.user_id,
+        tasks.title,
+        tasks.detail,
+        tasks.task_done,
+        tasks.task_archived,
+      ]
+    );
+  }
+
 }
 
 module.exports = TaskManager;
