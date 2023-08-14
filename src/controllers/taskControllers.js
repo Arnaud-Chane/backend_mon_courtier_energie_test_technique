428a58527ea8e0c63cbdcec0a2caf7aeac00322d
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-14 15:41:25 +0200

    Adding getTaskById in task controller

--- src/controllers/taskControllers.js
+++ src/controllers/taskControllers.js
@@ -14,6 +14,23 @@ const getAllTasks = (req, res) => {
     });
 };
 
+const getTaskById = (req, res) => {
+  models.task
+    .find(req.params.id)
+    .then(([task]) => {
+      if (task[0] == null) {
+        res.sendStatus(404);
+      } else {
+        res.send(task[0]);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   getAllTasks,
+  getTaskById,
 };
