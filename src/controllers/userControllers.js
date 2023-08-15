427c36b36189c303a131afc3195d6ca96f17ca22
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-15 15:23:33 +0200

    Adding method to get all user in controller

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,19 @@
+/* eslint-disable camelcase */
+/* eslint-disable prettier/prettier */
+const models = require("../models");
+
+const getAllUsers = (req, res) => {
+  models.user
+    .findAll()
+    .then(([user]) => {
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  getAllUsers,
+};
