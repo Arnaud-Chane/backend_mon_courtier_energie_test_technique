ceafecfb58d7b91feb4f882f6d4cbff8a9a86975
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-15 15:09:55 +0200

    Adding method addUser

--- src/models/UserManager.js
+++ src/models/UserManager.js
@@ -4,6 +4,13 @@ class UserManager extends AbstractManager {
   constructor() {
     super({ table: "user" });
   }
+
+  addUser(users) {
+    return this.database.query(
+      `insert into ${this.table} ( pseudo, email, password, is_admin ) values (?, ?, ?, ?)`,
+      [users.pseudo, users.email, users.hashedPassword, users.is_admin]
+    );
+  }
 }
 
 module.exports = UserManager;
