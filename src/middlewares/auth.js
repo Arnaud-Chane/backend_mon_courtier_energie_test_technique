26112a39c3b5947cf3d68554e0e352348f92f5b2
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-15 20:08:14 +0200

    Adding verify token to auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -45,7 +45,32 @@ const verifyPassword = (req, res) => {
     });
 };
 
+const verifyToken = (req, res, next) => {
+  try {
+    const authorizationHeader = req.get("Authorization");
+
+    if (authorizationHeader == null) {
+      throw new Error("Authorization header is missing");
+    }
+
+    const [type, token] = authorizationHeader.split(" ");
+
+    if (type !== "Bearer") {
+      throw new Error("Authorization header has not the 'Bearer' type");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.user_id = decodedToken.user_id;
+
+    next();
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(401);
+  }
+};
+
 module.exports = {
   hashPassword,
   verifyPassword,
+  verifyToken,
 };
