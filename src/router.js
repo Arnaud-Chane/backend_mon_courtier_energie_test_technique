5b6f6726186368a0aa014df084a536a21b63b7a1
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-08-15 15:40:33 +0200

    Adding route to delete user

--- src/router.js
+++ src/router.js
@@ -9,6 +9,7 @@ router.get("/users", userControllers.getAllUsers);
 router.get("/users/:id", userControllers.getUserById);
 router.post("/users", userControllers.createUser);
 router.put("/users/:id", userControllers.updateUser);
+router.delete("/users/:id", userControllers.deleteUser);
 
 router.get("/users/:id/tasks", taskControllers.getTaskByUserId);
 router.get("/tasks", taskControllers.getAllTasks);
